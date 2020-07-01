8e43f47d94c39cd21819f0674f4ad7d950f8e78d
Eline Grantangen <224948@usn.no>
2020-07-01 13:44:31 +0200

    set data in table

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component{
     this.state = {
       items: [],
       isLoaded: false,
-    }
+    };
   }
  
 componentDidMount(){
@@ -19,46 +19,55 @@ componentDidMount(){
   {method: 'GET', 
   headers: {'x-api-key' : '7a115e89bb8b4d43bd20255171b885c3' },
   }).then(res => res.json())
-    .then(json => console.log(json));
+    .then(banan => {
+      this.setState({
+        isLoaded: true,
+        items: banan
+      });
+    });
 }
 
-// fetch('/currencies/v1/convert/EUR?', {
-//   method: 'GET',
-//   headers: { 'x-api-key': '0b2a29cc474445a382b9c96176922740' },
-//   }).then(response => response.json())
-//   .then(json => console.log(json));
+
+// To show te result in json ==  .then(json => console.log(json));
 
 
 
   render() {
-    var { isLoaded, items } = this.state;
-    if (!isLoaded) {
-      return <div> Loading... </div>;
+    const {items} = this.state;
+    // if (!isLoaded) {
+    //   return <div> Loading ...</div>;
+    // } else {
+      return (
+        <table>
+          <thead>
+          <tr>
+            <th>Country</th>
+            <th>BaseCurrency</th>
+            <th>QuoteCurrency</th>
+            <th>Rate</th>
+          </tr>
+          </thead>
+          <tbody>
+          {items.map(item => (
+            <tr key={items.country}>
+              <td>{item.country}</td>
+              <td>{item.baseCurrency}</td>
+              <td>{item.quoteCurrency}</td>
+              <td>{item.midRate}</td>
+            </tr>
+            
+          ))}
+          </tbody>
+        </table>
+      );
     }
-    else {
-    return (
-      console.log(items),
-      <div className="App">
-        <h1> Currency</h1>
-        Data has been uploaded.
-        </div>
-      
-    );
-    }
-
   }
-}
+
+     
+
+  
 
 export default App;
 
 
-// {items.map(item => {
-//   const{baseCurrency, quoteCurrency} = currency;
-//      return (
-//        <div key={baseCurrency}>
-//        <p>From : {item.baseCurrency}</p>
-//        <p>To : {item.baseCurrency}</p>
-//        </div>
-//      );
-// }
 
